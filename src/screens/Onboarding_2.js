9b4855bcceddda0d83ca6b29d5dcaf3fbcbd9086
BB545 <black1016@hanmail.net>
2024-06-01 09:49:58 +0900

    docs: 온보딩 코드 수정

--- src/screens/Onboarding_2.js
+++ src/screens/Onboarding_2.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { SafeAreaView } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 
-const Onboarding_2 = () => {
+const Onboarding_2 = ({navigation}) => {
   return (
     <SafeAreaView style={{
       backgroundColor: '#ffffff'
@@ -14,6 +14,7 @@ const Onboarding_2 = () => {
             fontWeight: '900',
             color: '#000000',
             padding: 20,
+            marginTop: 100,
           }}>우리 함께 일상을 공유해요!</Text>
         </View>
         <View>
@@ -21,16 +22,18 @@ const Onboarding_2 = () => {
             color: '#000000',
             paddingLeft: 20,
             paddingRight: 20,
+            fontSize: 19
           }}>산책, 반려동물, 건강 정보 등 좋아하는 것에 대해 함께 이야기하고, 정보를 공유해요</Text>
         </View>
         <View style={{
           alignItems: 'center',
+          marginTop: 20,
         }}>
             <Image source={require('../../assets/img/Onboarding2.png')}
             style={{
               width: '100%',
-              height: 580,
-              marginTop: 10
+              height: '80%',
+              resizeMode: 'contain',
             }} />
           <TouchableOpacity style={{
             backgroundColor: 'orange',
@@ -39,9 +42,10 @@ const Onboarding_2 = () => {
             justifyContent: 'center',
             alignItems: 'center',
             position: 'fixed',
-            bottom: 150,
+            bottom: 180,
             borderRadius: 10,
-          }}>
+          }}
+          onPress={() => navigation.navigate('Onboarding_3')}>
             <Text style={{
               color: '#ffffff',
               fontSize: 20,
